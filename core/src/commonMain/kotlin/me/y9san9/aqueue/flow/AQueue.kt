3aa1a48b876845f5c3efd7bea5be7561e1bfdd06
y9san9 <y9san9@gmail.com>
2024-08-18 23:03:40 +0300

    feat: added launchInAQueue extension for flow

--- core/src/commonMain/kotlin/me/y9san9/aqueue/flow/AQueue.kt
+++ core/src/commonMain/kotlin/me/y9san9/aqueue/flow/AQueue.kt
@@ -1,8 +1,11 @@
 package me.y9san9.aqueue.flow
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.launch
 import me.y9san9.aqueue.AQueue
 import kotlin.coroutines.CoroutineContext
@@ -30,4 +33,23 @@ public fun <T, R> Flow<T>.mapInAQueue(
             }
         }
     }
+}
+
+/**
+ * Parallels flow using provided [queue] and launches it
+ *
+ * @param scope The scope used to launch flow
+ * @param key It is guaranteed that requests with the same [key] will be executed consecutively
+ * @param context The context that is used to launch new coroutines. You may limit parallelism using context
+ * @param queue The queue used to parallel flow
+ * @param action The action to perform with request
+ */
+public fun <T> Flow<T>.launchInAQueue(
+    scope: CoroutineScope,
+    key: (T) -> Any? = { null },
+    queue: AQueue = AQueue(),
+    context: CoroutineContext = EmptyCoroutineContext,
+    action: suspend (T) -> Unit
+): Job {
+    return mapInAQueue(key, queue, context, action).launchIn(scope)
 }
